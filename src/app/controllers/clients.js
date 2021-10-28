20df62d0df976b7ec3f4958026e97c368342d9c2
T-hash06 <tomasfelipe.p@gmail.com>
2021-10-28 12:52:18 -0500

    [CREATE] Actualizacion y Eliminacion de Clientes

--- src/app/controllers/clients.js
+++ src/app/controllers/clients.js
@@ -31,7 +31,6 @@ const createItem = async (req, res) => {
 			httpConflictError(res, Object.keys(e.keyValue)[0]);
 			return;
 		}
-
 		if (e.errors !== null) {
 			httpBadRequestError(res, Object.keys(e.errors));
 			return;
@@ -40,7 +39,32 @@ const createItem = async (req, res) => {
 	}
 };
 
-const updateItem = (req, res) => {};
-const deleteItem = (req, res) => {};
+const updateItem = async (req, res) => {
+	try {
+		const { cedula, telefono, nombre, email, direccion } = req.body;
+		const details = await clientModel.findOneAndUpdate({ cedula }, { cedula, telefono, nombre, email, direccion });
+		res.sendStatus(details !== null ? 200 : 404);
+	} catch (e) {
+		if (e.code === 11000) {
+			httpConflictError(res, Object.keys(e.keyValue)[0]);
+			return;
+		}
+		if (e.errors !== null) {
+			httpBadRequestError(res, Object.keys(e.errors));
+			return;
+		}
+		httpServerError(res, e);
+	}
+};
+
+const deleteItem = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const details = await clientModel.findOneAndDelete({ cedula: id });
+		res.sendStatus(details !== null ? 200 : 404);
+	} catch (e) {
+		httpServerError(res, e);
+	}
+};
 
 module.exports = { getItem, getItems, createItem, updateItem, deleteItem };
